0061fd055a23a00e683520a1c9f5397a4c296dd5
plebbyg <plebeiusgaragicus@gmail.com>
2023-04-15 19:19:09 -0700

    renamed, green, random raindrop start point

--- public/rain.js
+++ public/rain.js
@@ -19,15 +19,16 @@ const columns = canvas.width / fontSize;
 const rainDrops = [];
 
 for (let x = 0; x < columns; x++) {
-    rainDrops[x] = 1;
+    rainDrops[x] = Math.floor(Math.random() * canvas.height / fontSize);
 }
 
 const draw = () => {
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = '#FFA500'; // ORANGE
-    // context.fillStyle = '#0F0';
+    // context.fillStyle = '#FFA500'; // ORANGE is FFA500
+    // context.fillStyle = '#00FF00'; // GREEN is 00FF00
+    context.fillStyle = '#0F0'; // #0F0 is GREEN
     context.font = fontSize + 'px monospace';
 
     for (let i = 0; i < rainDrops.length; i++) {
